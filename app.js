213000798ba4fa8bb18ab32742b471b1e1f2e447
vietnt03 <viet2000333@gmail.com>
2023-03-11 15:00:30 +0700

    update_ 11_3_2023

--- app.js
+++ app.js
@@ -1,12 +1,106 @@
-const express = require('express');
+import express from "express";
+import axios from "axios";
 const app = express();
-const port = 3000
-app.get('/', (req, res) =>{
-    res.end('hello ')
-})
-app.get('/about', (req, res) =>{
-    res.end('hello world')
-})
-app.listen(port, () =>{
-    console.log(`ứng dụng đang chạy  http://localhost:${port}`);
-})
+const port = 8000;
+
+app.use(express.json());
+
+// GET LIST
+app.get("/products", async (req, res) => {
+  try {
+    const { data: products } = await axios.get(
+      "http://localhost:3001/products"
+    );
+    if (products.length === 0) {
+      res.send({
+        messenger: "Danh sách sản phẩm trống",
+      });
+    }
+    return res.json(products);
+  } catch (err) {
+    res.send({
+      messenger: err,
+    });
+  }
+});
+
+// GET DETAIL
+app.get("/products", async (req, res) => {
+  try {
+    const { data: product } = await axios.get(
+      `http://localhost:3001/products/${req.params.id}`
+    );
+    if (!product) {
+      res.send({
+        messenger: "Không tìm thấy sản phẩm",
+      });
+    }
+    return res.json(product);
+  } catch (err) {
+    res.send({
+      messenger: err,
+    });
+  }
+});
+
+// CREATE
+// app.post("/product", (req, res) => {
+//   products.push(req.body);
+//   res.status(201).send({
+//     messenger: "Them san pham thanh cong",
+//     data: products,
+//   });
+// });
+
+app.post("/products", async (req, res) => {
+  try {
+    const { data: product } = await axios.post(
+      `http://localhost:3001/products/`,
+      req.body
+    );
+
+    console.log(data);
+    // if (!product) {
+    //   res.send({
+    //     messenger: "Không tìm thấy sản phẩm",
+    //   });
+    // }
+    // return res.json(product);
+  } catch (err) {
+    res.send({
+      messenger: err,
+    });
+  }
+});
+
+//UPDATE
+app.put("/product/:id", (req, res) => {
+  const id = req.params.id;
+  const newProducts = products.filter((item) =>
+    item.id == id ? req.body : item
+  );
+  res.send({
+    messenger: "Thay doi thong tin san pham thanh cong",
+    data: newProducts,
+  });
+});
+
+// DELETE
+app.delete("product/:id", (req, res) => {
+  const id = req.params.id;
+  const newProducts = products.filter((item) => item.id != id);
+  res.send({
+    messenger: "Xoa san pham thanh cong",
+    data: newProducts,
+  });
+});
+app.listen(port, () => {
+  console.log(`ung dung dang chay vao file app tren port: ${port}`);
+});
+
+// Step 1: tạo file db.json
+// Step 2: cài đặt json-server.
+// Step 3: cài đặt concurrently
+// Step 4: config lại package.json
+// Step 5: install axios
+// Step 6: Code getlist, getdetails
