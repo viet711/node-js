595a42326eaa07c438647f9ccaee704a36855dc8
vietnt03 <viet2000333@gmail.com>
2023-03-19 16:30:44 +0700

    update

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,18 +1,18 @@
+
 import axios from "axios";
-// const API_URI = "http://localhost:3001/products/";
+const API_URI = "http://localhost:3001/products/";
+
 export const getAll = async (req, res) => {
   try {
-    const { data: products } = await axios.get(
-      "http://localhost:3001/products"
-    );
+    const { data: products } = await axios.get(API_URI);
     if (products.length === 0) {
-      res.send({
-        messenger: "Danh sách sản phẩm trống",
+      return res.send({
+        message: "Danh sách sản phẩm trống",
       });
     }
     return res.status(200).json(products);
-  } catch (err) {
-    res.status(500).json({ messenger: err });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 };
 
@@ -20,27 +20,37 @@ export const getDetail = async (req, res) => {
   try {
     const { data: product } = await axios.get(`${API_URI}${req.params.id}`);
     if (!product) {
-      res.send({
-        messenger: "Không tìm thấy sản phẩm",
+      return res.send({
+        message: "Không tìm thấy sản phẩm",
       });
     }
     return res.status(200).json(product);
-  } catch (err) {
-    res.status(500).json({ messenger: err });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 };
 
 export const create = async (req, res) => {
   try {
-    const { data: product } = await axios.post(`${API_URI}`, req.body);
+    if (
+      !req.body.name ||
+      !req.body.price ||
+      !req.body.description ||
+      !req.body.image
+    ) {
+      return res.status(400).json({
+        message: "Vui lòng cung cấp đầy đủ thông tin sản phẩm",
+      });
+    }
+    const { data: product } = await axios.post(API_URI, req.body);
     if (!product) {
-      res.send({
-        messenger: "Thêm sản phẩm không thành công",
+      return res.send({
+        message: "Thêm sản phẩm không thành công",
       });
     }
     return res.json(product);
-  } catch (err) {
-    res.status(500).json({ messenger: err });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 };
 
@@ -51,9 +61,7 @@ export const remove = async (req, res) => {
       message: "Sản phẩm đã được xóa thành công",
     });
   } catch (error) {
-    return res.status(500).json({
-      message: error,
-    });
+    return res.status(500).json({ message: error.message });
   }
 };
 
@@ -64,12 +72,29 @@ export const update = async (req, res) => {
       req.body
     );
     if (!product) {
-      res.send({
-        messenger: "Update sản phẩm không thành công",
+      return res.send({
+        message: "Update sản phẩm không thành công",
       });
     }
     return res.status(200).json(product);
-  } catch (err) {
-    res.status(500).json({ messenger: err });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+export const PATCH  = async (req, res) => {
+  try {
+    const { data: product } = await axios.patch(
+      `${API_URI}${req.params.id}`,
+      req.body
+    );
+    // console.log( product);
+    if (!product) {
+      return res.send({
+        message: "Cập nhật sản phẩm không thành công",
+      });
+    }
+    return res.status(200).json(product);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 };
--- src/router/product.js
+++ src/router/product.js
@@ -5,6 +5,7 @@ import {
   create,
   update,
   remove,
+  PATCH,
 } from "../controllers/product.js";
 
 const router = express.Router();
@@ -13,6 +14,7 @@ router.get("/products", getAll);
 router.get("/products/:id", getDetail);
 router.post("/products", create);
 router.put("/products/:id", update);
+router.patch("/products/:id", PATCH);
 router.delete("/products/:id", remove);
 
 export default router;
